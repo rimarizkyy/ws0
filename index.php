5d49287d911181c9e73c8a00086598b021ccaa93
kamal fauzan <kamalonnetx@gmail.com>
2016-03-21 23:58:46 +0700

    add keterangan

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php 
+<?php
+//Perhatikan alamat yang dituju oleh klien
+//SoapClient, http://localhost/latihan/ws0?wsdl
+
  $file='brosur_0.wsdl';
  if(isset($_GET["wsdl"])) readfile($file); 
 ?>
